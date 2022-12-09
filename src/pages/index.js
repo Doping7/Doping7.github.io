a942db0737545ff6d70cad6bc3f8ef2147fdca82
Doping7 <m271704@kakao.com>
2022-12-09 10:12:32 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function IndexPage() {
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['잡담']} />
         <AboutSection sectionId="about" heading="Write My Stack" />
-        <InterestsSection sectionId="details" heading="Details" />
+        {/*<InterestsSection sectionId="details" heading="Details" />*/}
         <ProjectsSection sectionId="features" heading="Built-in Features" />
         <ContactSection sectionId="github" heading="Contact" />
       </Page>
